30628271d0a4b6f1fcd65cdd678683b97ed39a01
Igor Astashov <astashovivl@gmail.com>
2023-12-21 18:07:20 +0700

    Edited main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from datetime import datetime
@@ -79,59 +79,56 @@ def root():
     return "Hello! This is our clinic's microservice for storing and updating information for dogs!"
 
 
-# 3. Реализован путь /post
-@app.post("/post", response_model=List[Timestamp])
-def get_post():
-    id_max = 0
-    for i in post_db:
-        if i.id > id_max:
-            id_max = i.id
-
-    new_id = id_max + 1
+@app.post("/post", response_model=Timestamp)
+def get_post(id_usr: int):
     current_time = int(datetime.now().timestamp())
-    new_usr = Timestamp(id=new_id, timestamp=current_time)
+    new_usr = Timestamp(id=id_usr, timestamp=current_time)
     post_db.extend([new_usr])
-    return [new_usr]
+    return new_usr
 
 
 # 4. Реализована запись собак
-@app.post("/dog", response_model=List[Dog])
+@app.post("/dog", response_model=Dog)
 def create_dog(name: str, kind: DogType):
     new_pk = max(dogs_db.keys()) + 1
     new_dog = Dog(name=name, pk=new_pk, kind=kind)
     dogs_db[new_pk] = new_dog
-    return [new_dog]
+    return new_dog
 
 
 # 5. Реализовано получение списка собак
-@app.get("/dogs_list")
-def get_dogs_list(limit: int = 1):
+@app.get("/dogs_list", response_model=List[Dog])
+def get_dogs_list(limit: int = 3):
     return list(dogs_db.values())[:limit]
 
 
 # 6. Реализовано получение собаки по id
 @app.get("/dog/{pk}", response_model=List[Dog])
 def get_dog_by_pk(dog_id: int):
+    if dog_id not in dogs_db:
+        raise HTTPException(status_code=404, detail='This pk is not exist')
     return [val for key, val in dogs_db.items() if key == dog_id]
 
 
 # 7. Реализовано получение собак по типу
-@app.get("/dog/")
-def get_dogs(dogType: DogType):
-    current_dogs = []
+@app.get("/dog/", response_model=List[Dog])
+def get_dogs(dogType: DogType = None):
+    if dogType is None:
+        return list(dogs_db.values())
 
-    for key, val in dogs_db.items():
+    current_dogs = []
+    for _, val in dogs_db.items():
         if val.kind == dogType:
             current_dogs.append(val)
     return current_dogs
 
 
 # 8. Реализовано обновление собаки по id
-@app.patch("/dog/{pk}", response_model=List[Dog])
-def update_dog(pk: int, new_name: str):
-    current_dog = []
-    for key, val in dogs_db.items():
-        if val.pk == pk:
-            val.name = new_name
-            current_dog.append(val)
-    return current_dog
+@app.patch("/dog/{pk}", response_model=Dog)
+def update_dog(pk: int, dog: Dog):
+    if pk not in dogs_db:
+        raise HTTPException(status_code=404, detail='This pk is not exist')
+    if pk != dog.pk:
+        raise HTTPException(status_code=404, detail='Pk does not match index')
+    dogs_db[pk] = dog
+    return dog
